cdc4b8adafcdad2e6c2790b06a1894a4a7475def
Ermakov Nikita <vienn1009@gmail.com>
2023-08-28 20:15:50 +0300

    AntD used

--- src/components/Movie-list/Movie-Item/Movie-item.js
+++ src/components/Movie-list/Movie-Item/Movie-item.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Card, Tag, Col, Row } from 'antd';
+import { Card, Tag, Col, Row } from "antd";
 import { format } from 'date-fns';
 
 export default function MovieItem(props) {
